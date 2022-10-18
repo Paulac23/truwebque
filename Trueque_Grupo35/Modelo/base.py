c31ab60a06187dbd6b5156280a9595cf8fbd7d11
bely092 <koen.lion92@gmail.com>
2022-10-17 21:28:20 -0300

    :white_check_mark: update

---
+++ Trueque_Grupo35/Modelo/base.py
@@ -0,0 +1,101 @@
+#importar lo que haga falta luego
+
+
+class BaseApp:
+    """
+    Clase administradora de la Base... 
+    """
+    def __init__(self) -> None:
+        pass
+    #creacion de la data base
+    #creacion de la conexion
+
+class Usuarios(BaseApp):
+    """
+    Clase con la tabla de usuarios
+    """
+    #llamar la conexion
+    #try crear tabla
+    #crear funcion de la tabla
+
+
+class UsuarioRoles(BaseApp):
+    """
+    Clase con la tabla de usuarios_roles
+    """
+    #llamar la conexion
+    #try crear tabla
+    #crear funcion de la tabla
+   
+    
+class Roles(BaseApp):
+    """
+    Clase con la tabla de roles
+    """
+    #llamar la conexion
+    # try crear tabla
+    #crear funcion de la tabla
+
+
+class Ciudades(BaseApp):
+    """
+    Clase con la tabla de ciudades
+    """
+    #llamar la conexion
+    # try crear tabla
+    #crear funcion de la tabla
+   
+    
+class Provincias(BaseApp):
+    """
+    Clase con la tabla de provincias
+    """
+    #llamar la conexion
+    # try crear tabla
+    #crear funcion de la tabla
+    
+    
+class Envios(BaseApp):
+    """
+    Clase con la tabla de envios
+    """
+    #llamar la conexion
+    # try crear tabla
+    #crear funcion de la tabla
+    
+    
+class Comentarios(BaseApp):
+    """
+    Clase con la tabla de comentarios
+    """
+    #llamar la conexion
+    # try crear tabla
+    #crear funcion de la tabla
+    
+    
+class Productos(BaseApp):
+    """
+    Clase con la tabla de productos
+    """
+    #llamar la conexion
+    # try crear tabla
+    #crear funcion de la tabla
+   
+    
+class Carrito(BaseApp):
+    """
+    Clase con la tabla de carritos
+    """
+    #llamar la conexion
+    # try crear tabla
+    #crear funcion de la tabla
+
+
+class Ordenes(BaseApp):
+    """
+    Clase con la tabla de ordenes
+    """
+    #llamar la conexion
+    # try crear tabla
+    #crear funcion de la tabla
+    

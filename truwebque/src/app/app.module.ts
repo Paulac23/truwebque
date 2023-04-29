faa5741aaad5484cfd3be92c33a5bcd2798247a5
bely092 <koen.lion92@gmail.com>
2023-04-29 16:06:13 -0300

    :rocket: Proyecto Angular

---
+++ truwebque/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { PagesComponent } from './pages/pages.component';
+import { ShopComponent } from './shop/shop.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthComponent,
+    PagesComponent,
+    ShopComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

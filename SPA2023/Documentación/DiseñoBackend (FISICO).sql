eb8ed554bfefcda292ce618f8c26a9bc6c0465ac
Roberto-Osores <osoresroberto@gmail.com>
2023-05-06 01:57:00 -0300

    Corregidos errores en los diseños logico y fisico

--- SPA2023/Documentación/DiseñoBackend (FISICO).sql
+++ SPA2023/Documentación/DiseñoBackend (FISICO).sql
@@ -5,122 +5,101 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema trueque2
+-- Schema mydb
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema trueque2
+-- Schema mydb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `trueque2` DEFAULT CHARACTER SET utf8mb3 ;
-USE `trueque2` ;
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
 
 -- -----------------------------------------------------
--- Table `trueque2`.`categorias`
+-- Table `mydb`.`suscripcion`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trueque2`.`categorias` (
-  `idcategoria` INT NOT NULL,
-  `nombre` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`idcategoria`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `trueque2`.`Suscripcion`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trueque2`.`Suscripcion` (
-  `idSuscripcion` INT NOT NULL,
-  `fecha_inicio` DATETIME NULL,
-  `fecha_fin` DATETIME NULL,
-  PRIMARY KEY (`idSuscripcion`))
+CREATE TABLE IF NOT EXISTS `mydb`.`suscripcion` (
+  `idsuscripcion` INT NOT NULL,
+  `fecha_inicio` DATETIME NOT NULL,
+  `fecha_fin` DATETIME NOT NULL,
+  PRIMARY KEY (`idsuscripcion`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `trueque2`.`personas`
+-- Table `mydb`.`personas`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trueque2`.`personas` (
+CREATE TABLE IF NOT EXISTS `mydb`.`personas` (
   `idpersonas` INT NOT NULL,
   `nombres` VARCHAR(45) NOT NULL,
   `apellido` VARCHAR(45) NOT NULL,
-  `dni` VARCHAR(45) NOT NULL,
+  `dni` INT NOT NULL,
   `fecha_alta` DATETIME NOT NULL,
-  `fecha_mod` DATETIME NULL DEFAULT NULL,
+  `fecha_mod` DATETIME NOT NULL,
   `fecha_nac` DATETIME NOT NULL,
   `direccion` VARCHAR(45) NOT NULL,
   `ciudad` INT NOT NULL,
   `provincia` INT NOT NULL,
   `correo` VARCHAR(45) NOT NULL,
-  `telefono` VARCHAR(45) NULL DEFAULT NULL,
-  `idSuscripcion` INT NOT NULL,
-  `idSuscripcion` INT NOT NULL,
+  `telefono` INT NOT NULL,
   `usuario` VARCHAR(45) NOT NULL,
   `contraseña` VARCHAR(45) NOT NULL,
-  `rol_admin` TINYINT NOT NULL,
+  `rol_admin` TINYINT(10) NOT NULL,
+  `suscripcion_idsuscripcion` INT NOT NULL,
   PRIMARY KEY (`idpersonas`),
-  INDEX `fk_personas_Suscripcion1_idx` (`idSuscripcion` ASC) VISIBLE,
-  INDEX `fk_personas_Suscripcion2_idx` (`idSuscripcion` ASC) VISIBLE,
-  CONSTRAINT `fk_personas_Suscripcion1`
-    FOREIGN KEY (`idSuscripcion`)
-    REFERENCES `mydb`.`Suscripcion` (`idSuscripcion`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_personas_Suscripcion2`
-    FOREIGN KEY (`idSuscripcion`)
-    REFERENCES `trueque2`.`Suscripcion` (`idSuscripcion`)
+  UNIQUE INDEX `idpersonas_UNIQUE` (`idpersonas` ASC) VISIBLE,
+  UNIQUE INDEX `dni_UNIQUE` (`dni` ASC) VISIBLE,
+  INDEX `fk_personas_suscripcion1_idx` (`suscripcion_idsuscripcion` ASC) VISIBLE,
+  CONSTRAINT `fk_personas_suscripcion1`
+    FOREIGN KEY (`suscripcion_idsuscripcion`)
+    REFERENCES `mydb`.`suscripcion` (`idsuscripcion`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `trueque2`.`publicaciones`
+-- Table `mydb`.`publicacion`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trueque2`.`publicaciones` (
+CREATE TABLE IF NOT EXISTS `mydb`.`publicacion` (
   `idpublicacion` INT NOT NULL,
-  `categoria` INT NOT NULL,
-  `usuario` INT NOT NULL,
   `estado` TINYINT NOT NULL,
   `titulo` VARCHAR(45) NOT NULL,
-  `descripcion` VARCHAR(1000) NOT NULL,
+  `descripcion` VARCHAR(45) NOT NULL,
   `fecha_publicacion` DATETIME NOT NULL,
-  `fecha_mod` DATETIME NULL DEFAULT NULL,
-  `cantidad` VARCHAR(45) NOT NULL,
-  `foto` BLOB NULL DEFAULT NULL,
-  `preferencias` VARCHAR(100) NOT NULL,
+  `fecha_mod` DATETIME NULL,
+  `cantidad` INT NOT NULL,
+  `foto` BLOB NULL,
+  `preferencias` VARCHAR(45) NOT NULL,
+  `categoria` INT NOT NULL,
+  `personas_idpersonas1` INT NOT NULL,
   PRIMARY KEY (`idpublicacion`),
-  INDEX `categoria_idx` (`categoria` ASC) VISIBLE,
-  INDEX `usuario_idx` (`usuario` ASC) VISIBLE,
-  CONSTRAINT `categoria`
-    FOREIGN KEY (`categoria`)
-    REFERENCES `trueque2`.`categorias` (`idcategoria`),
-  CONSTRAINT `usuario`
-    FOREIGN KEY (`usuario`)
-    REFERENCES `trueque2`.`personas` (`idpersonas`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
+  INDEX `fk_publicacion_personas1_idx` (`personas_idpersonas1` ASC) VISIBLE,
+  CONSTRAINT `fk_publicacion_personas1`
+    FOREIGN KEY (`personas_idpersonas1`)
+    REFERENCES `mydb`.`personas` (`idpersonas`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `trueque2`.`carrito`
+-- Table `mydb`.`carrito`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `trueque2`.`carrito` (
+CREATE TABLE IF NOT EXISTS `mydb`.`carrito` (
   `idcarrito` INT NOT NULL,
-  `carritocol` VARCHAR(45) NULL,
-  `idpublicacion` INT NOT NULL,
-  `idpersonas` INT NOT NULL,
-  PRIMARY KEY (`idcarrito`, `idpublicacion`, `idpersonas`),
-  INDEX `fk_carrito_publicaciones1_idx` (`idpublicacion` ASC) VISIBLE,
-  INDEX `fk_carrito_personas1_idx` (`idpersonas` ASC) VISIBLE,
-  CONSTRAINT `fk_carrito_publicaciones1`
-    FOREIGN KEY (`idpublicacion`)
-    REFERENCES `trueque2`.`publicaciones` (`idpublicacion`)
+  `publicacion_idpublicacion` INT NOT NULL,
+  `personas_idpersonas` INT NOT NULL,
+  PRIMARY KEY (`idcarrito`),
+  INDEX `fk_carrito_publicacion1_idx` (`publicacion_idpublicacion` ASC) VISIBLE,
+  INDEX `fk_carrito_personas1_idx` (`personas_idpersonas` ASC) VISIBLE,
+  CONSTRAINT `fk_carrito_publicacion1`
+    FOREIGN KEY (`publicacion_idpublicacion`)
+    REFERENCES `mydb`.`publicacion` (`idpublicacion`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_carrito_personas1`
-    FOREIGN KEY (`idpersonas`)
-    REFERENCES `trueque2`.`personas` (`idpersonas`)
+    FOREIGN KEY (`personas_idpersonas`)
+    REFERENCES `mydb`.`personas` (`idpersonas`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;

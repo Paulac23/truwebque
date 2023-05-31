aaea9ddb13f4a608c19dde4b0f8632385858dd13
Paula Cabrera <paucabrera95@gmail.com>
2023-05-30 22:37:28 -0300

    login form validation

--- SPA2023/Frontend/truwebque/src/app/auth/login/login.component.ts
+++ SPA2023/Frontend/truwebque/src/app/auth/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 
 @Component({
@@ -13,18 +15,34 @@ export class LoginComponent implements OnInit {
   public users: any = [];
   show = true;
 
+  constructor(public authService: AuthService, private readonly fb: FormBuilder) {};
 
-constructor(public authService: AuthService) {}
+  loginForm!: FormGroup;
 
-ngOnInit() {
+  email= new FormControl('');
+  
+
+onSubmit(): void{
+    console.log ('Form ->');
+  }
+
+ngOnInit(): void {
   this.authService.getUser().subscribe(resp => {
-    console.log('ta corriendo verdad?');
-    this.users = resp.data;
- });
+  console.log('ta corriendo verdad?');
+  this.users = resp.data;
 
+  this.loginForm = this.initForm();
+
+ })
 
 }
 
 
+initForm(): FormGroup{
+  return this.fb.group({
+    email: ['', [Validators.required, Validators.minLength(6), Validators.email]],
+  })
+
+}
 
 }
